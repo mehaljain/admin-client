198e9e72129b75defb517e9a61c895af91229f44
mehal jain <madhurjain2480@gmail.com>
2025-09-28 22:47:00 +0530

    commin index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-import { HashRouter } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';  // <-- Import BrowserRouter
 
-<HashRouter>
-  <App />
-</HashRouter>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>  {/* <-- Wrap your app */}
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+reportWebVitals();
